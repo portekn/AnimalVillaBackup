8e8017a42ebe21399d91856d5871744c7becb7aa
Kelli Porter <portekn@mail.uc.edu>
2022-07-10 15:36:19 -0400

    Adding Project to GitHub
    Adding base files to GitHub

---
+++ app/src/main/java/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.animalvilla.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
